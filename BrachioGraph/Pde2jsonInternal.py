3aacb46395b5af71b88e500a116beef43a53955a
justincong <justincong324@gmail.com>
2019-11-03 17:08:49 -0500

    change

--- BrachioGraph/Pde2jsonInternal.py
+++ BrachioGraph/Pde2jsonInternal.py
@@ -3,5 +3,5 @@ from .linedraw import *
 import shutil
 
 def create_json():
-    newPath = shutil.copy('.DrawSomethingKeys/SaveDraw.jpg', 'images')
+    newPath = shutil.copy('./DrawSomethingKeys/SaveDraw.jpg', 'images')
     image_to_json("SaveDraw.jpg")
