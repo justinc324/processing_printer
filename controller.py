c7c8d713eb07711aea546e81e979bedb2e98054f
justincong <justincong324@gmail.com>
2019-11-03 15:55:03 -0500

    f

--- controller.py
+++ controller.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # initialize the BrachioGraph
-bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-14, 1, 10, 15])
+bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-10, 3, 6, 12])
 
 while True:
 	button_state = GPIO.input(23)
