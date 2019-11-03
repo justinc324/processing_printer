3d72993a45aa7ddcf8bd61b78035c45e527fdf2e
justincong <justincong324@gmail.com>
2019-11-03 17:20:41 -0500

    fe

--- BrachioGraph/controller.py
+++ BrachioGraph/controller.py
@@ -1,19 +1,20 @@
 from brachiograph import BrachioGraph
 from Pde2jsonInternal import *
 
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# initialize the BrachioGraph
-bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-10, 3, 6, 12])
-
-while True:
-	button_state = GPIO.input(23)
-	if button_state == False:
-		create_json()
-		bg.plot_file("SaveDraw.jpg.json")
-		time.sleep(4)
+# import RPi.GPIO as GPIO
+# import time
+#
+# GPIO.setmode(GPIO.BCM)
+#
+# GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#
+# # initialize the BrachioGraph
+# bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-10, 3, 6, 12])
+#
+# while True:
+# 	button_state = GPIO.input(23)
+# 	if button_state == False:
+# 		create_json()
+# 		bg.plot_file("SaveDraw.jpg.json")
+# 		time.sleep(4)
+create_json()
