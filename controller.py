7889270580ab6acaf644d8ad58d9d03f603541bf
justincong <justincong324@gmail.com>
2019-11-03 15:31:30 -0500

    button

--- controller.py
+++ controller.py
@@ -1,3 +1,21 @@
 from BrachioGraph.brachiograph import BrachioGraph
+#from linedraw import *
 
-bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-14, 1, 10, 15])
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+	while True:
+		button_state = GPIO.input(23)
+		if button_state == False:
+			intitalize()
+			time.sleep(1)
+except:
+	GPIO.cleanup()
+
+def initialize():
+	bg = BrachioGraph(inner_arm=8, outer_arm=9.5, bounds=[-14, 1, 10, 15])
