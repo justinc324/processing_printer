3c9b64840b1a4f74714ea7a42925fe527efe1314
justincong <justincong324@gmail.com>
2019-11-04 00:15:38 -0500

    flag

--- BrachioGraph/controller.py
+++ BrachioGraph/controller.py
@@ -18,10 +18,14 @@ bg = BrachioGraph(inner_arm=8, outer_arm=9.5,
 				  bounds=[-10, 3, 6, 12],
 				  pw_up=1100, pw_down=800)
 
-while True:
-	button_state = GPIO.input(23)
-	if button_state == False:
-		create_json()
-		bg.plot_file("images/SaveDraw.jpg.json")
-		flag.ChangeDutyCycle(7.5)
-		time.sleep(4)
+try:
+	while True:
+		button_state = GPIO.input(23)
+		if button_state == False:
+			create_json()
+			bg.plot_file("images/SaveDraw.jpg.json")
+			flag.ChangeDutyCycle(7.5)
+			time.sleep(4)
+except KeyboardInterrupt:
+	flag.stop()
+	GPIO.cleanup()
