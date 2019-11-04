0cc3358fbcb5d5ccf6fa7d9b46b9c2236a9bf880
justincong <justincong324@gmail.com>
2019-11-03 23:49:44 -0500

    flag

--- BrachioGraph/controller.py
+++ BrachioGraph/controller.py
@@ -7,6 +7,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(8, GPIO.OUT)
+
+flag = GPIO.PWM(8, 50)
+
+flag.start(2.5)
 
 # initialize the BrachioGraph
 bg = BrachioGraph(inner_arm=8, outer_arm=9.5,
@@ -18,5 +23,5 @@ while True:
 	if button_state == False:
 		create_json()
 		bg.plot_file("images/SaveDraw.jpg.json")
-
+		flag.changeDutyCycle(7.5)
 		time.sleep(4)
